b8279117d0934353073b43ca8fd91d71a3311a63
Noga Anaby <nogaanaby@gmail.com>
2018-10-14 16:59:36 +0300

    ff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
+const path = require('path');
 const quotes = require('./routes/api/quotes')
 const services = require('./routes/api/services')
 
@@ -22,6 +22,16 @@ mongoose.connect(db, { useNewUrlParser: true } )
 //Use routes
 app.use('/api/quotes', quotes)
 app.use('/api/services', services)
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //listen on port
 const port = process.env.PORT || 5000;
